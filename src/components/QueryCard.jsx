053c2eb36e1e889d755f54a3e916e871cd87886d
Your Name <golamsarwar1896@gmail.com>
2024-12-23 03:18:38 +0600

    showed query products in the home page. Only shown 6 cards and only latest 6 posts will be shown.

--- src/components/QueryCard.jsx
+++ src/components/QueryCard.jsx
@@ -1,8 +1,44 @@
 const QueryCard = ({ query }) => {
-  const { product_name } = query || {};
+  const {
+    product_name,
+    product_brand,
+    query_title,
+    product_photo,
+    recommendedCount,
+    time,
+  } = query || {};
   return (
-    <div>
-      <h1>{product_name}</h1>
+    <div className="m-5">
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <figure className="p-3">
+          <img
+            className="w-96 rounded-xl h-80 object-cover"
+            src={product_photo}
+            alt="Shoes"
+          />
+        </figure>
+        <div className=" items-center text-center p-3">
+          <h2 className="text-3xl font-bold">{product_name}</h2>
+          <p className="px-3 py-2 w-1/2 mx-auto bg-primaryColor text-secondaryColor mt-2 font-medium">
+            Brand : {product_brand}
+          </p>
+          <h2 className="text-lg mt-5 w-84 mx-auto font-medium">
+            <span className="bg-primaryColor text-secondaryColor px-3 py-1 font-medium">
+              Queries:
+            </span>{" "}
+            {query_title}
+          </h2>
+
+          <div className="card-actions flex justify-between items-center mt-5">
+            <button className="btn bg-primaryColor text-secondaryColor rounded-3xl px-7 text-base ">
+              Add Query
+            </button>
+            <p className="px-3 py-1 bg-primaryColor text-secondaryColor mt-2 font-medium rounded-xl">
+              Recommended {recommendedCount} times
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/QueryCards.jsx
+++ src/components/QueryCards.jsx
@@ -9,19 +9,26 @@ const QueryCards = () => {
       const { data } = await axios.get(
         `${import.meta.env.VITE_API_URL}/add-queries`
       );
-      console.log(data);
       const sortedQueries = data.sort((a, b) => b.time - a.time);
-      console.log(sortedQueries);
       setQueries(sortedQueries);
+      console.log(sortedQueries);
     };
     fetchAllQuery();
   }, []);
-  console.log(queries);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-      {queries.map((query) => (
-        <QueryCard key={query._id} query={query}></QueryCard>
-      ))}
+    <div className="mt-20 text-center">
+      <div className="space-y-4">
+        <h1 className="text-5xl font-bold">Pick From Our Feature Page</h1>
+        <p>
+          We offer you the best products and best recommendation so that you
+          don't have to worry about your next purchase.
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 my-12">
+        {queries.slice(0, 6).map((query) => (
+          <QueryCard key={query._id} query={query}></QueryCard>
+        ))}
+      </div>
     </div>
   );
 };
