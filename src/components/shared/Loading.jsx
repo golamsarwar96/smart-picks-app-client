47bb297cf123a13300b387caaddc3384c8564c54
Your Name <golamsarwar1896@gmail.com>
2024-12-26 05:44:42 +0600

    changed the loading component icon

--- src/components/shared/Loading.jsx
+++ src/components/shared/Loading.jsx
@@ -1,9 +1,14 @@
-import React from "react";
-
+import Lottie from "lottie-react";
+import loadingAnimation from "../../assets/loadingAnim.json";
 const Loading = () => {
   return (
-    <div>
-      <span className="loading loading-spinner loading-lg"></span>
+    <div className="flex justify-center items-center md:mt-[180px]">
+      <Lottie
+        animationData={loadingAnimation}
+        style={{ width: "350px", height: "350px" }}
+        autoplay={true}
+        loop={true}
+      ></Lottie>
     </div>
   );
 };
